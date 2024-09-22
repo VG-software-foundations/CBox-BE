1bf1a9f0fbee2009dd9d2853c30f5fb26e526846
VictorGalkevich <2vektor54@gmail.com>
2024-09-22 22:04:13 +0300

    CI

--- src/main/java/com/example/cbox/CBoxApplication.java
+++ src/main/java/com/example/cbox/CBoxApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SuppressWarnings("")
 public class CBoxApplication {
 
     public static void main(String[] args) {
