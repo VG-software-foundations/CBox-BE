377263af451ea412d76ea8545d07827a737be62f
VictorGalkevich <2vektor54@gmail.com>
2024-09-23 15:48:17 +0300

    test

--- src/main/java/com/example/cbox/CBoxApplication.java
+++ src/main/java/com/example/cbox/CBoxApplication.java
@@ -1,13 +1,16 @@
 package com.example.cbox;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CBoxApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CBoxApplication.class, args);
+        log.info("Hello World!");
     }
 
 }
